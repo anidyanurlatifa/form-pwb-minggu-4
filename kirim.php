062ea2f18690bd328bd3f59f413dbccbd5f736cd
anidyanurlatifa <113292614+anidyanurlatifa@users.noreply.github.com>
2022-10-02 15:51:16 +0700

    Add files via upload

---
+++ kirim.php
@@ -0,0 +1,28 @@
+<?php
+   
+   if (isset($_POST['kirim'])); {
+    $fnim=$_POST['number'];
+    $fnama=$_POST['text'];
+    $fprodi=$_POST['""'];
+    $ftgl_lhr=$_POST['date'];
+    $fhobby1=$_POST['checkbox'];
+    $ffile=$_POST['file'];
+    
+
+    echo "======Ini Data Yang Di Input======";
+    echo"<br>";
+    echo $number;
+    echo"<br>";
+    echo $text;
+    echo"<br>";
+    echo $"";
+    echo"<br>";
+    echo $date;
+    echo"<br>";
+    echo $checkbox;
+    echo"<br>";
+    echo $file;
+    echo"<br>";
+
+   }
+?>
